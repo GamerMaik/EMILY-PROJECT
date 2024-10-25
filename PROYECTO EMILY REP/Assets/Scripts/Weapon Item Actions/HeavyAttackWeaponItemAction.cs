b9edbc11544daade554d6251ce1c2ba928274f39
GamerMaik <gammermaikyt@gmail.com>
2024-10-24 20:36:23 -0400

    parte final Two hand weapon
    Esta es la primera parte de la ultima de el control de ataque a dos manos de armas

--- PROYECTO EMILY REP/Assets/Scripts/Weapon Item Actions/HeavyAttackWeaponItemAction.cs
+++ PROYECTO EMILY REP/Assets/Scripts/Weapon Item Actions/HeavyAttackWeaponItemAction.cs
@@ -5,8 +5,12 @@ namespace KC
     [CreateAssetMenu(menuName = "Character Actions/Weapon Actions/Heavy Attack Action")]
     public class HeavyAttackWeaponItemAction : WeaponItemActions
     {
+        //Main Hand
         [SerializeField] string heavy_Attack_01 = "Main_Heavy_Attack_01";
         [SerializeField] string heavy_Attack_02 = "Main_Heavy_Attack_02";
+        //Two Hand
+        [SerializeField] string th_heavy_Attack_01 = "TH_Heavy_Attack_01";
+        [SerializeField] string th_heavy_Attack_02 = "TH_Heavy_Attack_02";
 
         public override void AttempToPerformAction(PlayerManager playerPerformingAction, WeaponItem weaponPerformingAction)
         {
@@ -30,6 +34,18 @@ namespace KC
 
         private void PerformHeavyAttack(PlayerManager playerPerformingAction, WeaponItem weaponPerformingAction)
         {
+            if (playerPerformingAction.playerNetworkManager.isTwoHandingWeapon.Value)
+            {
+                PerformTwoHandHeavyAttack(playerPerformingAction, weaponPerformingAction);
+            }
+            else
+            {
+                PerformMainHandHeavyAttack(playerPerformingAction, weaponPerformingAction);
+            }
+        }
+
+        private void PerformMainHandHeavyAttack(PlayerManager playerPerformingAction, WeaponItem weaponPerformingAction)
+        {
             if (playerPerformingAction.playerCombatManager.canComboWithMainHandWeapon && playerPerformingAction.isPerformingAction)
             {
                 playerPerformingAction.playerCombatManager.canComboWithMainHandWeapon = false;
@@ -47,6 +63,27 @@ namespace KC
             else if (!playerPerformingAction.isPerformingAction)
             {
                 playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.HeavyAttack01, heavy_Attack_01, true);
+            }
+        }
+        private void PerformTwoHandHeavyAttack(PlayerManager playerPerformingAction, WeaponItem weaponPerformingAction)
+        {
+            if (playerPerformingAction.playerCombatManager.canComboWithMainHandWeapon && playerPerformingAction.isPerformingAction)
+            {
+                playerPerformingAction.playerCombatManager.canComboWithMainHandWeapon = false;
+
+                //Realizar un ataque en base al ataque anterior
+                if (playerPerformingAction.characterCombatManager.lastAttackAnimationPerformed == th_heavy_Attack_01)
+                {
+                    playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.HeavyAttack02, th_heavy_Attack_02, true);
+                }
+                else
+                {
+                    playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.HeavyAttack01, th_heavy_Attack_01, true);
+                }
+            }
+            else if (!playerPerformingAction.isPerformingAction)
+            {
+                playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.HeavyAttack01, th_heavy_Attack_01, true);
             }
         }
     }
--- PROYECTO EMILY REP/Assets/Scripts/Weapon Item Actions/LightAttackWeaponItemAction.cs
+++ PROYECTO EMILY REP/Assets/Scripts/Weapon Item Actions/LightAttackWeaponItemAction.cs
@@ -1,10 +1,11 @@
-using UnityEngine;
+ using UnityEngine;
 
 namespace KC
 {
     [CreateAssetMenu(menuName = "Character Actions/Weapon Actions/Light Attack Action")]
     public class LightAttackWeaponItemAction : WeaponItemActions
     {
+        //Main Hand
         [Header("Light Attacks")]
         [SerializeField] string light_Attack_01 = "Main_Light_Attack_01";
         [SerializeField] string light_Attack_02 = "Main_Light_Attack_02";
@@ -13,6 +14,16 @@ namespace KC
         [SerializeField] string run_Attack_01 = "Main_Run_Attack_01";
         [SerializeField] string roll_Attack_01 = "Main_Roll_Attack_01";
         [SerializeField] string backstep_Attack_01 = "Main_Backstep_Attack_01";
+
+        //Two Hand
+        [Header("Light Attacks")]
+        [SerializeField] string th_light_Attack_01 = "TH_Light_Attack_01";
+        [SerializeField] string th_light_Attack_02 = "TH_Light_Attack_02";
+
+        [Header("Actions Attacks")]
+        [SerializeField] string th_run_Attack_01 = "TH_Run_Attack_01";
+        [SerializeField] string th_roll_Attack_01 = "TH_Roll_Attack_01";
+        [SerializeField] string th_backstep_Attack_01 = "TH_Backstep_Attack_01";
 
         public override void AttempToPerformAction(PlayerManager playerPerformingAction, WeaponItem weaponPerformingAction)
         {
@@ -56,6 +67,18 @@ namespace KC
 
         private void PerformLightAttack(PlayerManager playerPerformingAction, WeaponItem weaponPerformingAction)
         {
+            if (playerPerformingAction.playerNetworkManager.isTwoHandingWeapon.Value)
+            {
+                PerformTwoHandLightAttack(playerPerformingAction, weaponPerformingAction);
+            }
+            else
+            {
+                PerformMainHandLightAttack(playerPerformingAction, weaponPerformingAction);
+            }
+        }
+
+        private void PerformMainHandLightAttack(PlayerManager playerPerformingAction, WeaponItem weaponPerformingAction)
+        {
             if (playerPerformingAction.playerCombatManager.canComboWithMainHandWeapon && playerPerformingAction.isPerformingAction)
             {
                 playerPerformingAction.playerCombatManager.canComboWithMainHandWeapon = false;
@@ -63,43 +86,82 @@ namespace KC
                 //Realizar un ataque en base al ataque anterior
                 if (playerPerformingAction.characterCombatManager.lastAttackAnimationPerformed == light_Attack_01)
                 {
-                    playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction,AttackType.LightAttack02, light_Attack_02, true);
+                    playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.LightAttack02, light_Attack_02, true);
                 }
                 else
                 {
-                    playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction,AttackType.LightAttack01, light_Attack_01, true);
+                    playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.LightAttack01, light_Attack_01, true);
                 }
             }
             else if (!playerPerformingAction.isPerformingAction)
             {
-                playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction,AttackType.LightAttack01, light_Attack_01, true);
+                playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.LightAttack01, light_Attack_01, true);
             }
-
         }
+        private void PerformTwoHandLightAttack(PlayerManager playerPerformingAction, WeaponItem weaponPerformingAction)
+        {
+            if (playerPerformingAction.playerCombatManager.canComboWithMainHandWeapon && playerPerformingAction.isPerformingAction)
+            {
+                playerPerformingAction.playerCombatManager.canComboWithMainHandWeapon = false;
 
+                //Realizar un ataque en base al ataque anterior
+                if (playerPerformingAction.characterCombatManager.lastAttackAnimationPerformed == th_light_Attack_01)
+                {
+                    playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.LightAttack02, th_light_Attack_02, true);
+                }
+                else
+                {
+                    playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.LightAttack01, th_light_Attack_01, true);
+                }
+            }
+            else if (!playerPerformingAction.isPerformingAction)
+            {
+                playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.LightAttack01, th_light_Attack_01, true);
+            }
+        }
         private void PerformRunningAttack(PlayerManager playerPerformingAction, WeaponItem weaponPerformingAction)
         {
-            //Si tenemos el ataqu de dos manos reproducimos el ataque de dos manos
-            // Y si no, realizamos el ataque ligero
-            playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction,AttackType.RunningAttack01, run_Attack_01, true);
-
-
+            //Si tenemos el ataque de dos manos reproducimos el ataque de dos manos
+            if (playerPerformingAction.playerNetworkManager.isTwoHandingWeapon.Value)
+            {
+                playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.RunningAttack01, th_run_Attack_01, true);
+            }
+            else
+            {
+                // Y si no, realizamos el ataque ligero
+                playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.RunningAttack01, run_Attack_01, true);
+            }
         }
 
         private void PerformRollingAttack(PlayerManager playerPerformingAction, WeaponItem weaponPerformingAction)
         {
-            //Si tenemos el ataqu de dos manos reproducimos el ataque de dos manos
-            // Y si no, realizamos el ataque ligero
             playerPerformingAction.playerCombatManager.canPerformRollingAttack = false;
-            playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction,AttackType.RollingAttack01, roll_Attack_01, true);
+            //Si tenemos el ataqu de dos manos reproducimos el ataque de dos manos
+            if (playerPerformingAction.playerNetworkManager.isTwoHandingWeapon.Value)
+            {
+                playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.RollingAttack01, th_roll_Attack_01, true);
+            }
+            // Y si no, realizamos el ataque ligero
+            else
+            {
+                playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.RollingAttack01, roll_Attack_01, true);
+            }
+          
         }
 
         private void PerformBackStepAttack(PlayerManager playerPerformingAction, WeaponItem weaponPerformingAction)
         {
-            //Si tenemos el ataqu de dos manos reproducimos el ataque de dos manos
-            // Y si no, realizamos el ataque ligero
             playerPerformingAction.playerCombatManager.canPerformBackStepAttack = false;
-            playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction,AttackType.BackstepAttack01, backstep_Attack_01, true);
+            //Si tenemos el ataqu de dos manos reproducimos el ataque de dos manos
+            if (playerPerformingAction.playerNetworkManager.isTwoHandingWeapon.Value)
+            {
+                playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.BackstepAttack01, th_backstep_Attack_01, true);
+            }
+            // Y si no, realizamos el ataque ligero
+            else
+            {
+                playerPerformingAction.playerAnimatorManager.PlayerTargetAttackActionAnimation(weaponPerformingAction, AttackType.BackstepAttack01, backstep_Attack_01, true);
+            }
         }
     }
 }
